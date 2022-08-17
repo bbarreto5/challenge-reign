b94823eb18a9029a3aa3bc063e0d46a8b21710af
Brandon Barreto lozano <bbarreto@MacBook-Pro-de-Brandon.local>
2022-08-17 18:37:21 -0500

    create file App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import './App.css'
 import Bar from "./Components/Bar";
+import Button from "./Components/Button";
+import Container from "./Components/Container";
+
 
 
 function App() {
   return (
     <div className="App">
       <Bar/>
+      <Container>
+        <div className="container_buttons">
+          <Button name="ALL"/>
+          <Button name="My faves"/>
+        </div>
+        <div className=''>
+
+        </div>
+        <div className=''>
+
+        </div>
+      </Container>
     </div>
   );
 }
