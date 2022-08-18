dbf112f9b1bd6b8cb67c3aebe1ec918ae307bebe
Brandon Barreto lozano <bbarreto@MacBook-Pro-de-Brandon.local>
2022-08-18 00:57:48 -0500

    list data

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,22 @@ import './App.css'
 import Bar from "./Components/Bar";
 import Button from "./Components/Button";
 import Container from "./Components/Container";
+import Card from './Components/Card';
 import { getData } from "./Services";
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 
 interface IData {
   author: string,
   story_title: string,
   story_url: string,
-  reated_at: string,
+  created_at: string,
 }
 
 
 function App() {
 
-  const [page, setPage] = useState<number>(0);
+  const [page, setPage] = useState<number>(-1);
   const [totalPages, setTotalPage] = useState<number>(0);
   const [type, setType] = useState<1 | 2 | 3>(1);
   const [data, setData] = useState<Array<IData>>([]);
@@ -27,17 +30,29 @@ function App() {
       author: d.author,
       story_title: d.story_title,
       story_url: d.story_url,
-      reated_at: d.reated_at,
+      created_at: d.created_at,
     }))
+    console.log({ auxData });
     return auxData;
   }
 
   useEffect(() => {
-    getData(1, page).then(data => setData(dataFilter(data)));
+    setPage(-1);
+    getData(type, 0).then(data => {
+      setTotalPage(data.nbPages);
+      setData(dataFilter(data))
+    });
   }, [])
 
+  useEffect(() => {
+    if (page != -1) {
+      getData(type, page).then(data => setData(dataFilter(data)));
+    }
+  }, [page])
 
-
+  const handlePagination = (_: any, newPage: number) => {
+    setPage(--newPage);
+  }
 
   return (
     <div className="App">
@@ -49,9 +64,17 @@ function App() {
         </div>
         <div className=''>
 
-        </div>
-        <div className=''>
+          <div className='list_article'>
+            {
+              data.map( (d:IData) => <Card date={d.created_at} description={d.story_title}/>)
+            }
+          </div>
 
+        </div>
+        <div className='container_pagination'>
+          <Stack spacing={2}>
+            <Pagination count={totalPages} variant="outlined" shape="rounded" onChange={handlePagination} />
+          </Stack>
         </div>
       </Container>
     </div>
